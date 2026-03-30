ad3c5c65c98798ff55cb34fa7d5b228924ae2bd1
murilo arevalo <muriloarevalo@gmail.com>
2026-03-30 00:41:57 -0300

    finalizacao do codigo/desfio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Digite o seu numero: ");
+        int numero = sc.nextInt();
+        sc.nextLine();
+        System.out.printf("Digite a sua agencia: ");
+        String agencia = sc.nextLine();
+        System.out.printf("Digite o seu nome: ");
+        String nomeCliente = sc.nextLine();
+        System.out.printf("Digite o seu saldo: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque.");
+
     }
 }
