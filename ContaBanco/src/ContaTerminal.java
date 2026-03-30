5fd16f55dee3f61a204942f602606fe29b122f29
murilo arevalo <muriloarevalo@gmail.com>
2026-03-30 00:43:49 -0300

    Limpei o codigo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+
         System.out.printf("Digite o seu numero: ");
         int numero = sc.nextInt();
         sc.nextLine();
+
         System.out.printf("Digite a sua agencia: ");
         String agencia = sc.nextLine();
+
         System.out.printf("Digite o seu nome: ");
         String nomeCliente = sc.nextLine();
+        
         System.out.printf("Digite o seu saldo: ");
         double saldo = sc.nextDouble();
 
